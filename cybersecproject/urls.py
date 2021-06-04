10728cf6b5c9c4836d2a29c67dfef7c9e08c51d0
jpasikainen <jp.asikainen@pm.me>
2021-06-04 13:54:22 +0300

    Added link to admin tools page

--- cybersecproject/urls.py
+++ cybersecproject/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from .views import home_view, login_view, register_view, logout_view, submit_post_view, profile_view
 
 urlpatterns = [
+    path('admin', admin.site.urls),
     path('', home_view, name='home'),
     path('login', login_view, name='login'),
     path('register', register_view, name='register'),
